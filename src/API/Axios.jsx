ef2023d293044a5d7e43452e2cf04833ee433f71
WabiDagim <wabidagim280@gmail.com>
2026-02-23 00:30:59 -1000

    erro for deployement

--- src/API/Axios.jsx
+++ src/API/Axios.jsx
@@ -1,11 +1,9 @@
-when import axios from "axios";
+import axios from "axios";
 const instance = axios.create({
   //1 Local instance of the backend
   // baseURL: "http://127.0.0.1:5001/backend-22770/us-central1/api",
   //2 Render instance of the backend
   baseURL: "https://amazon-me-rd5s.onrender.com"
-  //3 Firebase instance of the backend
-  // baseURL: he is not working bacause i don't have card number  "
-  
+  //3 Firebase instance of the backend (not active)
 })
 export default instance;
